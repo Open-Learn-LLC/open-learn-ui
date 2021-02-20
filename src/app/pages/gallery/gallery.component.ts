855a13506c49d3aa04a62340d274c10d8ec71695
arichards4814 <arichards4814@gmail.com>
2021-02-20 18:39:46 -0500

    icon chooser

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -11,39 +11,43 @@ export class GalleryComponent implements OnInit {
   faTimes = faTimes;
   sizeOptions = ['Medium', 'Small', 'Large', 'Xtra-Large']
   colorOptions = ['Blue', 'Black', 'Teal', 'Brown']
-  sizeChosen = 'medium'; 
+  iconOptions = ['none', 'top-left', 'top-right', 'bottom-left', 'bottom-right']
+  sizeChosen = 'medium';  
   colorChosen = ''; 
-  code = `
-    <app-general-card 
-        size="${this.sizeChosen}" 
-        color="${this.colorChosen}"
-        title="Hello 123" 
-        subtitle="Testing Subtitle"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod teat cupidatat non proident, sunt in c."
-        footer="testing 123">
-    </app-general-card>
-  `
+  icon = 'none'
+  code: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.setCode()
   }
 
-  onColorChanged(e){
-    this.colorChosen = e.target.value.toLowerCase()
-    this.code = `
-        <app-general-card 
-            size="${this.sizeChosen}" 
-            color="${this.colorChosen}"
-            title="Hello 123" 
-            subtitle="Testing Subtitle"
-            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod teat cupidatat non proident, sunt in c."
-            footer="testing 123">
-        </app-general-card>
-  `
+  onIconChanged(e){
+    this.icon = e.target.value;
+    this.setCode()
   }
 
   onSizeChanged(e) {
-    this.sizeChosen = e.target.value.toLowerCase()
+    this.sizeChosen = e.target.value.toLowerCase();
+    this.setCode();
+  }
+
+  onColorChanged(e) {
+    this.colorChosen = e.target.value.toLowerCase();
+    this.setCode();
+  }
+
+  setCode(){
+    let iconHtml: string;
+    
+    if(this.icon != 'none'){
+      iconHtml = `
+       <fa-icon class="${this.icon}" size="2x" [icon]="faTimes"></fa-icon>         
+    `
+    } else {
+      iconHtml = ``
+    }
+    
     this.code = `
         <app-general-card 
             size="${this.sizeChosen}" 
@@ -52,6 +56,7 @@ export class GalleryComponent implements OnInit {
             subtitle="Testing Subtitle"
             body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod teat cupidatat non proident, sunt in c."
             footer="testing 123">
+            ${iconHtml}
         </app-general-card>
   `
   }
